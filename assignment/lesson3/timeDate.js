7935a0f56fb4f38eb81ceca7e50ffa338beb5cba
etmasi <hesalwayswithme61@gmail.com>
2020-02-06 13:40:51 +0200

    message

--- assignment/lesson3/timeDate.js
+++
@@ -1,4 +0,0 @@
-function timeDate() {
-    document.getElementById("currentYear").innerHTML = new Date().getFullYear();
-    document.getElementById("lastChanged").innerHTML = "Last Updated: " + document.lastModified;
-}
---
+++ js/dates.js
@@ -0,0 +1,2 @@
+document.getElementById("thisyear").innerHTML = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = "Last Updated: " + document.lastModified;
